ca9acae015c923c663ec5ff22ef8223d7cd17cad
FrancoRosa <km115.franco@gmail.com>
2021-09-27 14:36:11 -0500

    Style area

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,31 +1,32 @@
 class AreasController < ApplicationController
-  before_action :set_area, only: %i[ show edit update destroy ]
+  before_action :set_area, only: %i[show edit update destroy]
 
   # GET /areas or /areas.json
   def index
     @areas = Area.all
+    @facilities = Facility.all
   end
 
   # GET /areas/1 or /areas/1.json
-  def show
-  end
+  def show; end
 
   # GET /areas/new
   def new
     @area = Area.new
+    @facilities = Facility.all
   end
 
   # GET /areas/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /areas or /areas.json
   def create
     @area = Area.new(area_params)
+    @facilities = Facility.all
 
     respond_to do |format|
       if @area.save
-        format.html { redirect_to @area, notice: "Area was successfully created." }
+        format.html { redirect_to @area, notice: 'Area was successfully created.' }
         format.json { render :show, status: :created, location: @area }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +39,7 @@ class AreasController < ApplicationController
   def update
     respond_to do |format|
       if @area.update(area_params)
-        format.html { redirect_to @area, notice: "Area was successfully updated." }
+        format.html { redirect_to @area, notice: 'Area was successfully updated.' }
         format.json { render :show, status: :ok, location: @area }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -51,19 +52,20 @@ class AreasController < ApplicationController
   def destroy
     @area.destroy
     respond_to do |format|
-      format.html { redirect_to areas_url, notice: "Area was successfully destroyed." }
+      format.html { redirect_to areas_url, notice: 'Area was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_area
-      @area = Area.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def area_params
-      params.require(:area).permit(:name, :location, :contact_name, :contact_phone, :facility_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_area
+    @area = Area.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def area_params
+    params.require(:area).permit(:name, :location, :contact_name, :contact_phone, :facility_id)
+  end
 end
