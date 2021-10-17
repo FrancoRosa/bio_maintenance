fc7a35f8d3040d88d88c8266f1fadc8f1255bb32
FrancoRosa <km115.franco@gmail.com>
2021-10-17 14:01:36 -0500

    Add react to project

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className="title">Title</p>
+      <p className="subtitle">La pucha exmpaos Title</p>
+      <p>Worked!!!</p>
+    </div>
+  );
+};
+
+export default App;
