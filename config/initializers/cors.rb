c572e1bdfbdc200f27b49cc0374f72ab841d5f42
FrancoRosa <km115.franco@gmail.com>
2021-11-11 21:36:45 -0500

    Update rules

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'localhost:4000', 'bio-peru.web.app'
+    origins 'localhost:4000', 'bio-peru.web.app', 'soluciones-biomedicas-peruanas.web.app'
     resource '*', headers: :any, methods: %i[get post]
   end
 end
