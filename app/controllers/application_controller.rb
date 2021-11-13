94e1a77157584c1a41d705de8d9ae09557f7ab50
FrancoRosa <km115.franco@gmail.com>
2021-11-12 22:05:01 -0500

    Display hostmane

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   def require_password_verification
+    p '-----------------------------------------'
+    p '-----------------------------------------'
+    p request.host
+    p '-----------------------------------------'
+    p '-----------------------------------------'
     return if request.host == 'soluciones-biomedicas-peruanas.web.app'
     return redirect_to '/login' unless cookies[:visitor_password_verified]
   end
