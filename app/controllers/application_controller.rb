304b4e5d7ce91cb9785634273380b21dde93b1ab
FrancoRosa <km115.franco@gmail.com>
2021-11-12 22:09:16 -0500

    Display referer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
     p '-----------------------------------------'
     p '-----------------------------------------'
     p request.host
+    p request.referrer
     p '-----------------------------------------'
     p '-----------------------------------------'
     return if request.host == 'soluciones-biomedicas-peruanas.web.app'
