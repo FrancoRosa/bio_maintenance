3e84678e308479e8c6101f0c911b618084e7d4af
FrancoRosa <km115.franco@gmail.com>
2021-11-12 22:25:47 -0500

    Fix return error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def require_password_verification
-    return if 'https://soluciones-biomedicas-peruanas.web.app' in request.referrer
+    return true if 'https://soluciones-biomedicas-peruanas.web.app' in request.referrer
     return redirect_to '/login' unless cookies[:visitor_password_verified]
   end
 end
