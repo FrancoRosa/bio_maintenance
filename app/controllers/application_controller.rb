56425a43fe7e3a6db4cc409f2fb7f8d34b044508
francorosa <km115.franco@gmail.com>
2021-12-03 18:48:19 -0500

    Receive date as parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   def require_password_verification
     source = request.referrer.to_s
     return if source.include? 'https://soluciones-biomedicas-peruanas.web.app'
-    return if source.include? 'localhost:4000'
+    # return if source.include? 'localhost:4000'
     return redirect_to '/login' unless cookies[:visitor_password_verified]
   end
 end
